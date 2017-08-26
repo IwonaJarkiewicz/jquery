37ae641aef06e2bb8c06ac0e71354e7c5120fb15
IwonaJarkiewicz <politea@gmail.com>
2017-08-26 13:40:08 +0200

    zdarzenia

--- js/4-find-each.js
+++ js/4-find-each.js
@@ -2,4 +2,9 @@
 $(function () {
     //wybieranie elementu z kolekcji za pomocą metody eq()
     $('body').find('p').eq(1) css('color', 'green');
+
+    //pętla each dodając do paragrafó klasę z kolejnym numerem
+    $('p').each(function (index) {
+        $(this).addClass('paragraf-' + index);
+    });
 });
